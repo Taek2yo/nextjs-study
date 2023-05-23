6d5e89c8698eb44fb82a1304e45b5614328362aa
Mintaek2 <789rkd@naver.com>
2023-05-23 22:28:30 +0900

    Part 1 끝

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,23 @@
+import {age, name} from './data'
+
 export default function Cart() {
-    return (
-      <div>
-        <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-      </div>
-    )
-  } 
+  let 장바구니 = ['Tomatoes', 'Pasta']
+  return (
+    <div>
+      <h4 className="title">Cart</h4>
+      {장바구니.map((item, idx) => {
+        return <CartItem item={item} key={idx}/>
+      })}
+    </div>
+  );
+}
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  );
+}
--- app/list/page.js
+++ app/list/page.js
@@ -1,26 +1,57 @@
+'use client'
+
+import { useState } from "react";
+
 export default function List() {
-  let products = [
+  const [products, setProducts] = useState([
     {
       name: "Tomatoes",
       price: "20",
+      수량: 0
     },
     {
       name: "Pasta",
       price: "30",
+      수량: 0
     },
     {
       name: "Coconut",
       price: "40",
+      수량: 0
     },
-  ];
+  ]);
+  
+  const increaseQuantity = (i) => {
+    const copy = [...products];
+    copy[i] = {
+      ...copy[i],
+      수량: copy[i].수량 + 1
+    };
+    setProducts(copy);
+  };
+
+  const decreaseQuantity = (i) => {
+    const copy = [...products];
+    if (copy[i].수량 > 0) {
+      copy[i] = {
+        ...copy[i],
+        수량: copy[i].수량 - 1
+      };
+      setProducts(copy);
+    }
+  };
+
   return (
     <div>
-      <h1 className="title">Products</h1>  
+      <h1 className="title">Products</h1>     
       {products.map((item, idx) => {
         return (
           <div className="food" key={idx}>
-            <img src={`/food${idx}.png`} className="food-img"/>
+            <img src={`/food${idx}.png`} className="food-img" alt={item.name}/>
             <h4>{item.name} ${item.price}</h4>
+            <button onClick={()=>{decreaseQuantity(idx)}}>-</button>
+            <span> {item.수량} </span>
+            <button onClick={()=>{increaseQuantity(idx)}}>+</button>
           </div>
         );
       })}
